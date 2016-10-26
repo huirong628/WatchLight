5ec3b8b227643e3ddea0053e687bc8569ce9e603
HuirongZhang <mrzhang628@gmail.com>
2016-10-26 17:41:42 +0800

    add custom Application

---
+++ app/src/main/java/com/qihoo360/antilostwatch/light/base/WatchApplication.java
@@ -0,0 +1,22 @@
+package com.qihoo360.antilostwatch.light.base;
+
+import android.app.Application;
+
+/**
+ * Created by HuirongZhang on 2016/10/26.
+ */
+
+public class WatchApplication extends Application {
+
+    private static WatchApplication mInstance;
+
+    public static WatchApplication getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+}
