e56fa5c9d663648a6ff82c999de7ff9c311ba68f
HuirongZhang <mrzhang628@gmail.com>
2016-11-14 09:36:03 +0800

    恢复为懒汉式单例，延迟加载和缓存

--- app/src/main/java/com/qihoo360/antilostwatch/light/ui/postlist/PostListFragment.java
+++ app/src/main/java/com/qihoo360/antilostwatch/light/ui/postlist/PostListFragment.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 
 public class PostListFragment extends BaseFragment<PostListContract.Presenter> implements PostListContract.View, OnRefreshListener, OnLoadMoreListener {
-    private static final PostListFragment mInstance = new PostListFragment();
+    private static PostListFragment mInstance = null;
     private SwipeToLoadLayout mSwipeToLoadLayout;
     private PostListAdapter mAdapter;
     private List<PostBean> mPostList = new ArrayList<>();
@@ -28,10 +28,14 @@ public class PostListFragment extends BaseFragment<PostListContract.Presenter> i
      * 单例模式可以保证系统中一个类只有一个实例.
      * <p>
      * 实例存在多个会引起程序逻辑错误的时候
-     *
+     * Do not place Android context classes in static fields (static reference to PostListFragment which has field mContext pointing to Context);
+     * this is a memory leak (and also breaks Instant Run)
      * @return
      */
     public static PostListFragment newInstance() {
+        if (mInstance == null) {
+            mInstance = new PostListFragment();
+        }
         return mInstance;
     }
 
