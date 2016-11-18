076bb623dc02ca400f8b4aac01ddca9f4fa8a8fa
HuirongZhang <mrzhang628@gmail.com>
2016-11-18 16:37:19 +0800

    添加新的接口类型

--- app/src/main/java/com/qihoo360/antilostwatch/light/api/ApiService.java
+++ app/src/main/java/com/qihoo360/antilostwatch/light/api/ApiService.java
@@ -3,10 +3,16 @@ package com.qihoo360.antilostwatch.light.api;
 
 import java.util.Map;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Url;
 import rx.Observable;
 
@@ -25,4 +31,10 @@ public interface ApiService {
 
     @GET
     Observable<Response<ResponseBody>> queryByGet(@HeaderMap Map<String, String> header, @Url String url);
+
+    @POST
+    Observable<Response<ResponseBody>> queryByPost(@HeaderMap Map<String, String> header, @Url String url, @Body RequestBody requestBody);
+
+    @Multipart
+    Observable<Response<ResponseBody>> queryByMultipart(@HeaderMap Map<String, String> header, @Url String url, @Part MultipartBody multipartBody);
 }
