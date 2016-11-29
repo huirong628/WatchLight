b611db0e9f547a5abb41991300ba10775d136f99
HuirongZhang <mrzhang628@gmail.com>
2016-11-29 18:44:31 +0800

    Custom TabView

--- app/src/main/java/com/qihoo360/antilostwatch/light/view/viewpagerindicator/TabView.java
+++ app/src/main/java/com/qihoo360/antilostwatch/light/view/viewpagerindicator/TabView.java
@@ -1,19 +1,39 @@
 package com.qihoo360.antilostwatch.light.view.viewpagerindicator;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.qihoo360.antilostwatch.light.R;
 
 /**
  * Created by HuirongZhang
  * on 2016/11/29.
  */
 
-public class TabView extends TextView {
+public class TabView extends LinearLayout {
+    private TextView mTabTitle;
+    private ImageView mTabIcon;
     private int index;
     private int maxTabWidth;
 
     public TabView(Context context) {
-        super(context);
+        this(context, null);
+    }
+
+    public TabView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        /**
+         * error here
+         *
+         *
+         */
+        LayoutInflater.from(context).inflate(R.layout.tab_view_layout, this, true);
+        mTabTitle = (TextView) findViewById(R.id.tab_title);
+        mTabIcon = (ImageView) findViewById(R.id.tab_icon);
     }
 
     @Override
@@ -37,5 +57,13 @@ public class TabView extends TextView {
 
     public void setMaxTabWidth(int maxTabWidth) {
         this.maxTabWidth = maxTabWidth;
+    }
+
+    public void setTitle(CharSequence text) {
+        mTabTitle.setText(text);
+    }
+
+    public void setImage(int resId) {
+        mTabIcon.setImageResource(resId);
     }
 }
